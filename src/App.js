78b5bbff8bb4f2aa941a8efaaa72c64f7cb86f90
d3metura <113028037+d3metura@users.noreply.github.com>
2023-03-19 07:48:03 -0700

    Experimenting with nested children components context

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { PureComponent } from "react";
 import "./App.css";
 import AuthContext, { AuthProvider } from "./AuthContext";
+import Child2 from "./Child3.js";
+import Child3 from "./Child3.js";
 
 export default class App extends PureComponent {
   render() {
@@ -8,25 +10,35 @@ export default class App extends PureComponent {
       <div>
         <h1>App Component</h1>
         <AuthProvider>
-          <Child />
+          <Child1 />
         </AuthProvider>
       </div>
     );
   }
 }
 
-class Child extends PureComponent {
+class Child1 extends PureComponent {
   render() {
-    console.log(this.context);
+    // console.log(this.context);
+    const { name, isAuthenticated, logIn, logOut } = this.context;
     return (
       <div>
         <h1>Child Component</h1>
+        <h2>User: {name}</h2>
+        <h2>
+          Authenticated: {isAuthenticated ? "Authenticated" : "Unauthenticated"}
+        </h2>
+        <button onClick={logIn}>Log In</button>
+        <button onClick={logOut}>Log Out</button>
+        <Child2 />
+        <Child3 />
       </div>
     );
   }
 }
 
 // 3 ways to use context
+//
 // 1. give child contextType => Child.contextType = AuthContext;
 
-Child.contextType = AuthContext;
+Child1.contextType = AuthContext;
